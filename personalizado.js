9ce73b05d6707fccad8d6210477c9151e40c27de
HugoAissa <112508131+HugoAissa@users.noreply.github.com>
2022-12-20 01:40:47 -0300

    Add files via upload

---
+++ personalizado.js
@@ -0,0 +1,49 @@
+function Daily() {
+    //WORK
+    document.getElementById("titulo").innerHTML = "Work"
+    document.getElementById("hrs").innerHTML = "5hrs"
+    document.getElementById("last").innerHTML = "Last Week = 7hrs"
+    //FIM WORK
+
+    //EXERCISE
+    document.getElementById("titulo2").innerHTML = "Exercise"
+    document.getElementById("hrs2").innerHTML = "1hrs"
+    document.getElementById("last2").innerHTML = "Last Week = 1hrs"
+    //FIM EXERCISE
+
+    //SOCIAL
+    document.getElementById("titulo3").innerHTML = "Social"
+    document.getElementById("hrs3").innerHTML = "1hrs"
+    document.getElementById("last3").innerHTML = "Last Week = 3hrs"
+    //FIM SOCIAL
+
+
+
+}
+
+
+function Weekly() {
+    //WORK
+    document.getElementById("titulo").innerHTML = "Work"
+    document.getElementById("hrs").innerHTML = "32hrs"
+    document.getElementById("last").innerHTML = "Last Week = 36hrs"
+    //FIM WORK
+
+    //EXERCISE
+    document.getElementById("titulo2").innerHTML = "Exercise"
+    document.getElementById("hrs2").innerHTML = "4hrs"
+    document.getElementById("last2").innerHTML = "Last Week = 5hrs"
+    //FIM EXERCISE
+
+    //SOCIAL
+    document.getElementById("titulo3").innerHTML = "Social"
+    document.getElementById("hrs3").innerHTML = "5hrs"
+    document.getElementById("last3").innerHTML = "Last Week = 10hrs"
+    //FIM SOCIAL
+
+}
+
+
+
+
+
